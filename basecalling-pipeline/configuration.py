1f5a844b44a2672e204b6f4d50278ff6ef229e4a
RodTol <rodolfo.tolloi@gmail.com>
2024-02-01 12:26:10 +0100

    first python

---
+++ basecalling-pipeline/configuration.py
@@ -0,0 +1,7 @@
+import os
+
+input_dir = os.environ.get("INPUT_DIR")
+print(f"Value of INPUT_DIR: {input_dir}")
+
+output_dir = os.environ.get("OUTPUT_DIR")
+print(f"Value of OUTPUT_DIR: {output_dir}")
