6ea006274dd52e7bc6d7d14b7bc140194aa4ec1b
RodTol <rodolfo.tolloi@gmail.com>
2024-02-02 16:29:55 +0100

    input output dir

---
+++ utility/pretty_jenkins.groovy
@@ -0,0 +1,34 @@
+def echoBanner(def ... msgs) {
+   echo createBanner(msgs)
+}
+
+def errorBanner(def ... msgs) {
+   error(createBanner(msgs))
+}
+
+def createBanner(def ... msgs) {
+   return """
+       ===========================================
+
+       ${msgFlatten(null, msgs).join("\n        ")}
+
+       ===========================================
+   """
+}
+
+// flatten function hack included in case Jenkins security
+// is set to preclude calling Groovy flatten() static method
+// NOTE: works well on all nested collections except a Map
+def msgFlatten(def list, def msgs) {
+   list = list ?: []
+   if (!(msgs instanceof String) && !(msgs instanceof GString)) {
+       msgs.each { msg ->
+           list = msgFlatten(list, msg)
+       }
+   }
+   else {
+       list += msgs
+   }
+
+   return  list
+}
