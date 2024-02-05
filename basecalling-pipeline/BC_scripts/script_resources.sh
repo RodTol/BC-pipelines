b495c87293760f9e220820864b6289dc2cd4e711
RodTol <rodolfo.tolloi@gmail.com>
2024-02-05 14:57:02 +0100

    try launching resources 6

--- basecalling-pipeline/BC_scripts/script_resources.sh
+++ basecalling-pipeline/BC_scripts/script_resources.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=jk-try
 #SBATCH --time=00:10:00
-#SBATCH -p DGX --ntasks-per-node=1 --nodes=1 --cpus-per-task=1 --gpus=0 --nodelist=dgx002
+#SBATCH -p DGX --ntasks-per-node=1 --nodes=1 --cpus-per-task=1 --gpus=0
 #SBATCH hetjob
 #SBATCH -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
-
-##SBATCH hetjob
-##SBATCH -p DGX --ntasks-per-node=1 --nodes=1 --cpus-per-task=256 --gpus=8 --nodelist=dgx001
-##SBATCH hetjob
-##SBATCH -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=48 --nodelist=gpu003
-##SBATCH hetjob
-##SBATCH -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=48 --nodelist=gpu002
-##SBATCH hetjob
-##SBATCH -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=48 --nodelist=gpu001
 
 
 srun --het-group=0 ~/BC-pipelines/utility/prova_job.sh &
