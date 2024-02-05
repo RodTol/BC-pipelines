0777eda6654ac5c4743724368e10bc9065c3b9fb
RodTol <rodolfo.tolloi@gmail.com>
2024-02-05 17:15:10 +0100

    adding parameter 2

--- basecalling-pipeline/configuration.py
+++ basecalling-pipeline/configuration.py
@@ -1,15 +1,27 @@
 import os
+import sys
 import json
 
-#Load in config the json file
-with open('config.json', 'r') as json_file:
-    config = json.load(json_file)
+def process_json(json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+        # Your logic to process the JSON data
+        return data
 
-run_name = config['General']['run_name']
-input_dir = config['Basecalling']['input_dir']
-output_dir = config['Basecalling']['output_dir']
-date = config["General"]["date"]
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Error!")
+        print("Usage: python script.py <json_file>")
+        sys.exit(1)
+    
+    json_file = sys.argv[1]
+    config = process_json(json_file)
 
-print(f"Value of INPUT_DIR: {input_dir}")
-print(f"Value of OUTPUT_DIR: {output_dir}")
-print(f"date for config.json: {date}")
+    run_name = config['General']['run_name']
+    input_dir = config['Basecalling']['input_dir']
+    output_dir = config['Basecalling']['output_dir']
+    date = config["General"]["date"]
+
+    print(f"Value of INPUT_DIR: {input_dir}")
+    print(f"Value of OUTPUT_DIR: {output_dir}")
+    print(f"date for config.json: {date}")
