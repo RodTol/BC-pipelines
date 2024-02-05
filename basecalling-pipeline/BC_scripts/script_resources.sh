c494ad7cc7f2ddd7a9f54e75037a77b81d4576bf
RodTol <rodolfo.tolloi@gmail.com>
2024-02-05 16:17:27 +0100

    try launching resources 8

--- basecalling-pipeline/BC_scripts/script_resources.sh
+++ basecalling-pipeline/BC_scripts/script_resources.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=jk-try
 #SBATCH --time=00:10:00
+#SBATCH --output=~/BC-pipelines/basecalling-pipeline/
 #SBATCH -p DGX --ntasks-per-node=1 --nodes=1 --cpus-per-task=1 --gpus=0
 #SBATCH hetjob
 #SBATCH -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
-#SBATCH --output=~/BC-pipelines/basecalling-pipeline/
 
 
 srun --het-group=0 ~/BC-pipelines/utility/prova_job.sh &
