a0dc41d7f7c5be92dd797914f200e8aef90e37d7
RodTol <rodolfo.tolloi@gmail.com>
2024-02-05 17:04:34 +0100

    try launching resources 10

--- basecalling-pipeline/configuration.py
+++ basecalling-pipeline/configuration.py
@@ -8,6 +8,8 @@ with open('config.json', 'r') as json_file:
 run_name = config['General']['run_name']
 input_dir = config['Basecalling']['input_dir']
 output_dir = config['Basecalling']['output_dir']
+date = config["General"]["date"]
 
 print(f"Value of INPUT_DIR: {input_dir}")
 print(f"Value of OUTPUT_DIR: {output_dir}")
+print(f"date for config.json: {date}")
