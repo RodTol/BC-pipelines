5cf479e5f15548611156c9af9b039d1526103d77
RodTol <rodolfo.tolloi@gmail.com>
2024-02-06 15:20:37 +0100

    slurm output file

--- basecalling-pipeline/configuration.py
+++ basecalling-pipeline/configuration.py
@@ -19,8 +19,6 @@ if __name__ == "__main__":
     run_name = config['General']['run_name']
     input_dir = config['Basecalling']['input_dir']
     output_dir = config['Basecalling']['output_dir']
-    date = config["General"]["date"]
 
     print(f"Value of INPUT_DIR: {input_dir}")
     print(f"Value of OUTPUT_DIR: {output_dir}")
-    print(f"date for config.json: {date}")
