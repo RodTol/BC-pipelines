a0a22ed754b33440f7eb4c51057fe650e47aa270
RodTol <rodolfo.tolloi@gmail.com>
2024-02-08 17:33:28 +0100

    reading config from json

--- basecalling-pipeline/BC_scripts/host/instructions.sh
+++ basecalling-pipeline/BC_scripts/host/instructions.sh
@@ -3,7 +3,10 @@ current_time=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Current time: $current_time"
 echo "hello from ${SLURM_JOB_NODELIST}"
 
+model=$1
+logs_dir=$2
+
 echo "Launching the server"
-~/BC-pipelines/basecalling-pipeline/BC_scripts/host/server.sh
+~/BC-pipelines/basecalling-pipeline/BC_scripts/host/server.sh $model $logs_dir
 
 
