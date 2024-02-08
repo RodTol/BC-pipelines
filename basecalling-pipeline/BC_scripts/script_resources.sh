dc5ba3785895b9f944056ab56071a54ae1385211
RodTol <rodolfo.tolloi@gmail.com>
2024-02-08 17:01:42 +0100

    tmp dir

--- basecalling-pipeline/BC_scripts/script_resources.sh
+++ basecalling-pipeline/BC_scripts/script_resources.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=jk-try
 #SBATCH --time=00:05:00
-#SBATCH --output=/u/area/jenkins_onpexp/jenkins_logs/%x-%j.out  # Use %x for job name and %j for job ID
-#SBATCH --error=/u/area/jenkins_onpexp/jenkins_logs/%x-%j.err   # Use %x for job name and %j for job ID
-#SBATCH -A lade -p DGX --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --gpus=1
+#SBATCH --output=/u/area/jenkins_onpexp/jenkins_logs/tmp/%x-%j.out  # Use %x for job name and %j for job ID
+#SBATCH --error=/u/area/jenkins_onpexp/jenkins_logs/tmp/%x-%j.err   # Use %x for job name and %j for job ID
+#SBATCH -p DGX --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --gpus=1
 #SBATCH hetjob
-#SBATCH -A lade -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=24
+#SBATCH -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=24
 
 echo "id"
 cd ~
