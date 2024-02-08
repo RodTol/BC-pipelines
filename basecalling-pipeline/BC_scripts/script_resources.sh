725931759a4b40da8436f7655a684a0485293214
RodTol <rodolfo.tolloi@gmail.com>
2024-02-08 16:57:32 +0100

    slurm cleanup

--- basecalling-pipeline/BC_scripts/script_resources.sh
+++ basecalling-pipeline/BC_scripts/script_resources.sh
@@ -3,9 +3,9 @@
 #SBATCH --time=00:05:00
 #SBATCH --output=/u/area/jenkins_onpexp/jenkins_logs/%x-%j.out  # Use %x for job name and %j for job ID
 #SBATCH --error=/u/area/jenkins_onpexp/jenkins_logs/%x-%j.err   # Use %x for job name and %j for job ID
-#SBATCH -p DGX --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --gpus=1
+#SBATCH -A lade -p DGX --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --gpus=1
 #SBATCH hetjob
-#SBATCH -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=24
+#SBATCH -A lade -p GPU --nodes=1 --ntasks-per-node=1 --cpus-per-task=24
 
 echo "id"
 cd ~
