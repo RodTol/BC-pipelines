a2cb65a9d4cb76aa1540b73e2468a282779fa1fd
RodTol <rodolfo.tolloi@gmail.com>
2024-02-08 11:59:48 +0100

    new user 5

--- basecalling-pipeline/configuration.py
+++ basecalling-pipeline/configuration.py
@@ -19,6 +19,9 @@ if __name__ == "__main__":
     run_name = config['General']['run_name']
     input_dir = config['Basecalling']['input_dir']
     output_dir = config['Basecalling']['output_dir']
+    logs_dir = config['Basecalling']['logs_dir']
 
     print(f"Value of INPUT_DIR: {input_dir}")
     print(f"Value of OUTPUT_DIR: {output_dir}")
+    print(f"Value of LOGS_DIR: {logs_dir}")
+
