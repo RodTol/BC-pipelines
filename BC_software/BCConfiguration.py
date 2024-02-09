f154e5c01aadfb62b2bb9ec9ab3bc0f7f6203e71
RodTol <rodolfo.tolloi@gmail.com>
2024-02-09 14:21:03 +0100

    back to ezio

--- BC_software/BCConfiguration.py
+++ BC_software/BCConfiguration.py
@@ -9,9 +9,6 @@ class Conf:
     mngt_outputdir = '/AB_20T_output/nanopore_output/BC_run_output/run_sup_1_dgx-4_gpu'
     mngt_inputdir = '/AB_20T_input/dataset/CliveOME_5mc_dataset_POD5'
 
-    #dgx001 hosting
-    #request_work_url =  "http://10.128.2.161:5000/assignwork"
-    #dgx002 hosting
     request_work_url =  "http://127.0.0.1:40765/assignwork"
     
     engine_external_script = "/u/dssc/tolloi/Cluster_Basecalling_Manager/BC_software/orfeo_executable/executable_dgx002/launch_supervisor.sh"
@@ -21,12 +18,5 @@ class Conf:
     engine_id = "dgx002_engine"
     engine_optimal_request_size = 111
     
-    #dgx001 hosting
-    #keep_alive_terminate_url =  "http://10.128.2.161:5000/completed"
-    #dgx002 hosting
     keep_alive_terminate_url = "http://127.0.0.1:40765/completed"
-    
-    #dgx001 hosting
-    #keep_alive_url =  "http://10.128.2.161:5000/keepalive"
-    #dgx002 hosting
     keep_alive_url = "http://127.0.0.1:40765/keepalive"
