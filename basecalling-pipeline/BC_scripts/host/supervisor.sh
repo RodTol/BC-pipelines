9bf2a441c6d10f4cc648881c40d2fbf1927d8108
RodTol <rodolfo.tolloi@gmail.com>
2024-02-09 17:39:06 +0100

    BCP with BCC reading from json 10

--- basecalling-pipeline/BC_scripts/host/supervisor.sh
+++ basecalling-pipeline/BC_scripts/host/supervisor.sh
@@ -17,5 +17,5 @@ num_clients=4
 $dorado_server_path/ont_basecaller_supervisor --num_clients $num_clients \
 --input_path $1 \
 --save_path $save_path \
---config dna_r10.4.1_e8.2_400bps_hac.cfg \
+--config dna_r10.4.1_e8.2_400bps_hac \
 --port 41757 
