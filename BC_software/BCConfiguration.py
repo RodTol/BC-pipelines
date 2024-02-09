a8da34aa5a682434eccea3b0b069f690270d0cc3
RodTol <rodolfo.tolloi@gmail.com>
2024-02-09 14:25:59 +0100

    creator for COnf with reading json

--- BC_software/BCConfiguration.py
+++ BC_software/BCConfiguration.py
@@ -6,17 +6,19 @@ class Conf:
     """
 
     mngt_batch_size = 3
-    mngt_outputdir = '/AB_20T_output/nanopore_output/BC_run_output/run_sup_1_dgx-4_gpu'
-    mngt_inputdir = '/AB_20T_input/dataset/CliveOME_5mc_dataset_POD5'
+    mngt_outputdir = ''
+    mngt_inputdir = ''
 
-    request_work_url =  "http://127.0.0.1:40765/assignwork"
+    request_work_url = 'http://127.0.0.1:40765/assignwork'
     
-    engine_external_script = "/u/dssc/tolloi/Cluster_Basecalling_Manager/BC_software/orfeo_executable/executable_dgx002/launch_supervisor.sh"
-    engine_outputdir = '/AB_20T_output/nanopore_output/BC_run_output/run_sup_1_dgx-4_gpu'
-    engine_inputdir = "/AB_20T_input/dataset/CliveOME_5mc_dataset_POD5"
+    engine_external_script = ''
+    engine_outputdir = ''
+    engine_inputdir = ''
     engine_polling_interval = 1
-    engine_id = "dgx002_engine"
-    engine_optimal_request_size = 111
+    engine_id = ''
+    engine_optimal_request_size = 100
     
     keep_alive_terminate_url = "http://127.0.0.1:40765/completed"
     keep_alive_url = "http://127.0.0.1:40765/keepalive"
+
+
