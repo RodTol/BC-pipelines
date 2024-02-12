5f2ae58ed382be18181ef1e0e6eea5469a42564e
RodTol <rodolfo.tolloi@gmail.com>
2024-02-12 17:21:56 +0100

    try to fix why supervisor script is not invoked with the right value for model | additional debugging

--- basecalling-pipeline/BC_scripts/host/server.sh
+++ basecalling-pipeline/BC_scripts/host/server.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+RESET="\033[0m"  # Reset color to default
+
 # Check if at least one argument is provided
 if [ "$#" -eq 0 ]; then
     echo "SERVER SCRIPT: Missing at least one argument"
     exit 1
 fi
 
-echo "HOST SCRIPT"
+echo "${GREEN}HOST SCRIPT${GREEN}"
 
 model="$1"
-echo "model $model"
+echo "${GREEN}model $model${RESET}"
 
 log_path="$2"
-echo "Log files path $log_path"
+echo -e "${GREEN}Log files path: ${log_path} ${RESET}"
 
 port=42837
-echo -e "${RED}Port${RESET}"
+echo -e "${GREEN}Port${RESET}"
 echo $port
 
 #Already added to path
