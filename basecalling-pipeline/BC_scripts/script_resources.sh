eac6086c39ae735e06c51de0e5fec5f4e60ce1e6
RodTol <rodolfo.tolloi@gmail.com>
2024-02-12 14:35:06 +0100

    First implementation: basecallign with scripts 2

--- basecalling-pipeline/BC_scripts/script_resources.sh
+++ basecalling-pipeline/BC_scripts/script_resources.sh
@@ -10,7 +10,10 @@
 
 json_file=$1
 
-srun --het-group=0 ~/BC-pipelines/basecalling-pipeline/BC_scripts/host/instructions.sh $json_file &
+#only one node
+srun ~/BC-pipelines/basecalling-pipeline/BC_scripts/host/instructions.sh $json_file &
+
+#srun --het-group=0 ~/BC-pipelines/basecalling-pipeline/BC_scripts/host/instructions.sh $json_file &
 sleep 10
 #run --het-group=1 ~/BC-pipelines/basecalling-pipeline/BC_scripts/client/instructions.sh $model $logs_dir &
 wait
