e13304c35ed2323401390a6391f2757332d6a6e4
RodTol <rodolfo.tolloi@gmail.com>
2024-02-13 11:56:48 +0100

    Try to capture jobid 9

--- BC_scripts/script_resources.sh
+++ BC_scripts/script_resources.sh
@@ -11,10 +11,6 @@
 json_file=$1
 index_host=$(jq -r '.Resources.index_host' "$json_file")
 
-JOB_ID=$SLURM_JOB_ID
-export JOB_ID
-
-
 #Only one node, launched with index for host node
 srun ~/BC-pipelines/BC_scripts/instructions.sh $json_file $index_host
 
