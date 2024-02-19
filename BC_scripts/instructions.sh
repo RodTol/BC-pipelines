cdef632cba4e4a4e29c10f8ccb97793c58958e54
RodTol <rodolfo.tolloi@gmail.com>
2024-02-19 16:14:27 +0100

    try with 2 dgx 1 gpu | debuggin queue parameter

--- BC_scripts/instructions.sh
+++ BC_scripts/instructions.sh
@@ -41,15 +41,15 @@ echo "Launching the server"
 ~/BC-pipelines/BC_scripts/server.sh $model $logs_dir $gpus_settings &
 sleep 5
 
-echo ""
-
 #Load virtualenv for python
 if ((node_queue == "DGX")); then
   source /u/area/jenkins_onpexp/python_venvs/DGX_dorado_venv/bin/activate
   echo "${node_name} is loading DGX venv"
+  pip freeze
 elif ((node_queue == "GPU")); then
   source /u/area/jenkins_onpexp/python_venvs/GPU_dorado_venv/bin/activate
   echo "${node_name} is loading GPU venv"
+  pip freeze
 else
   echo -e "${RED}SOMETHING WRONG IN THE VIRTUALENV FOR BC SOFTWARE${RESET}"
 fi
