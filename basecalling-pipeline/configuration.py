092c8284da5f1fa6802a9b2c93d0d424cca8838a
RodTol <rodolfo.tolloi@gmail.com>
2024-02-20 14:50:17 +0100

    first implementation of dynamic batch file written by config.py

--- basecalling-pipeline/configuration.py
+++ basecalling-pipeline/configuration.py
@@ -5,41 +5,50 @@ def load_json(json_file):
     with open(json_file, 'r') as file:
         data = json.load(file)
         return data
-    
+
+# Only need is to have host node as first one! INDEX HOST ALWAYS 0!
 def create_sbatch_file(config):
-    run_name = config['General']['run_name']
-    nodes_list = config['Resources']['nodes_list']
     # Define the content of the sbatch script
-    sbatch_content_1 = f'''#**********WRITTEN BY CONFIGURATION.PY**********
-#!/bin/bash
-#SBATCH --job-name={run_name}
-#SBATCH --time=00:20:00
-#SBATCH --output=/u/area/jenkins_onpexp/scratch/jenkins_logs/tmp/%x-%j.out  
-#SBATCH --error=/u/area/jenkins_onpexp/scratch/jenkins_logs/tmp/%x-%j.err  
-'''
-    
-    sbatch_content_2 = f'''    
-#SBATCH -A lage -p DGX --nodelist={nodes_list[0]} --nodes=1 --ntasks-per-node=1 --cpus-per-task=24 --gpus=2
-#SBATCH hetjob
-#SBATCH -A lage -p DGX --nodelist=extra_nodeeeeee --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
-'''
-    sbatch_content_3 = f'''
-json_file=$1
-index_host=$(jq -r '.Resources.index_host' "$json_file")
+    how_many_nodes = len(config['Resources']['nodes_list'])
 
-#Only one node, launched with index for host node
-#srun ~/BC-pipelines/BC_scripts/instructions.sh $json_file $index_host
-
-srun --het-group=0 ~/BC-pipelines/BC_scripts/instructions.sh $json_file $index_host
-sleep 10
-srun --het-group=1 ~/BC-pipelines/utility/prova.sh
-'''
-    # Write the content to the sbatch script file
     with open("script_resources.sh", "w") as sbatch_file:
-        sbatch_file.write(sbatch_content_1)
-        sbatch_file.write(sbatch_content_2)
-        sbatch_file.write(sbatch_content_3)
+        sbatch_file.write('#**********WRITTEN BY CONFIGURATION.PY**********\n')
+        sbatch_file.write('#!/bin/bash\n')
+        sbatch_file.write(f"#SBATCH --job-name={config['General']['run_name']}\n")
+        sbatch_file.write(f"#SBATCH --time={config['General']['run_time']}\n")
+        sbatch_file.write(f"#SBATCH --output={config['Slurm']['output']}\n")
+        sbatch_file.write(f"#SBATCH --error={config['Slurm']['error']}\n")
+        
+        sbatch_file.write("\n")
 
+        for i in range(how_many_nodes):
+            sbatch_file.write(f"#SBATCH -A lage -p {config['Resources']['nodes_queue'][i]}")
+            sbatch_file.write(f" --nodelist={config['Resources']['nodes_list'][i]}")
+            sbatch_file.write(f" --nodes=1 --ntasks-per-node=1")
+            sbatch_file.write(f" --cpus-per-task={config['Resources']['nodes_cpus'][i]}")
+            
+            if config['Resources']['nodes_gpus'][i] != "None":
+                sbatch_file.write(f" --gpus {config['Resources']['nodes_gpus'][i]}\n")
+            else:
+                sbatch_file.write("\n")
+
+            if i!=how_many_nodes-1:
+                sbatch_file.write("#SBATCH hetjob\n\n")
+            else :
+                sbatch_file.write("\n")
+
+        sbatch_file.write("\n")
+        
+        sbatch_file.write('json_file=$1\n')
+        sbatch_file.write("index_host=$(jq -r '.Resources.index_host' '$json_file')\n")
+
+        sbatch_file.write("\n")
+        for i in range(how_many_nodes):
+            sbatch_file.write(f"srun --het-group={i} {config['Slurm']['instructions']} $json_file $((index_host + {i})) &\n")
+            if i!=how_many_nodes-1:
+                sbatch_file.write("sleep 5\n")
+            else :
+                sbatch_file.write("wait\n")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -52,13 +61,13 @@ if __name__ == "__main__":
 
     create_sbatch_file(config)
 
-    model = config['Basecalling']['model']
-    input_dir = config['Basecalling']['input_dir']
-    output_dir = config['Basecalling']['output_dir']
-    logs_dir = config['Basecalling']['logs_dir']
+    # model = config['Basecalling']['model']
+    # input_dir = config['Basecalling']['input_dir']
+    # output_dir = config['Basecalling']['output_dir']
+    # logs_dir = config['Basecalling']['logs_dir']
     
-    print(f"Selected MODEL : {model}")
-    print(f"Value of INPUT_DIR: {input_dir}")
-    print(f"Value of OUTPUT_DIR: {output_dir}")
-    print(f"Value of LOGS_DIR: {logs_dir}")
+    # print(f"Selected MODEL : {model}")
+    # print(f"Value of INPUT_DIR: {input_dir}")
+    # print(f"Value of OUTPUT_DIR: {output_dir}")
+    # print(f"Value of LOGS_DIR: {logs_dir}")
 
