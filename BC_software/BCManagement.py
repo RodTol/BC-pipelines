603debf05e08320955acc56d0e7ca8acf9521d11
RodTol <rodolfo.tolloi@gmail.com>
2024-02-27 16:20:05 +0100

    BCManager commented

--- BC_software/BCManagement.py
+++ BC_software/BCManagement.py
@@ -44,44 +44,21 @@ class BCBatch:
 class BCWorkloadState:
     """
     Class that represents the state of the basecalling processing, as well as
-    providing methods for operating on it.
+    providing methods for operating on it.  
 
-    Essentially all the raw output from the Oxford Nano Pore sequencing machine
-    consists of .POD5 files that the machine writes to a specific directory we'll
-    refer to as INPUT_DIR.
-
-    Each raw file, is the recording of the electrical signal corresponding to the
-    particular DNA chunk that was sequenced. The chunks are known as frags, while the
-    recordings are also called squibbles because of the shape of the resulting graph.
-    Basically this raw data represents electrical current/potential over time.
-
-    For each file, a corresponding .FASTQ file must be produced and stored in
-    OUTPUT_DIR.
-
-    This is the basecalled file: a very long string of 4 letters representing the
-    biological bases, G A T C, that translates the raw electrical signals.
-
-    The actual basecalling functionality is carried out by a different software/system,
-    i.e. the DORADO system. But there are also other systems such as BONITO that can be used.
-    This software is needed because the algorithms involved in basecalling make use of
-    neural networks in order to translate the electrical signals, and so are rather
-    specialised for the task requiring also the availability of GPUs.
-
-    Several instances of the DORADO servers are expected to be available: at least one for each
-    GPU in the infrastructure. Moreover, each DORADO server is designed to work on multiple files
-    which it expects to be present in a specified directory.
-
-    The class allows raw files to be assigned to one of the DORADO server for processing. This
-    will be reflected in the filesystem by the presence of the following structure:
-
-    INPUT_DIR
-        |- JOB-ID1_DORADO-SERVER-NAME_inputdir
-            |- file1.pod5   (ATTENTION! IT WILL BE A SYMLINK!)
-            |- file2.pod5   (ATTENTION! IT WILL BE A SYMLINK!)
-            |- file3.pod5   (ATTENTION! IT WILL BE A SYMLINK!)
+    It consist of :
+    - INPUTDIR              path to the dir containing all the raw .POD5 files.
+    - OUTPUTDIR             path to the dir where the basecalling should direct its output for this job.
+    - unassigned_bc         list of file that still need to be basecalled
+    - default_batch_size    default size of the batch 
+    - assigned_batches      the actual batch of file
     """
-
     def __init__(self, json_file_path, node_index):
+        """
+        Initialize the BCMReader class with the provided JSON file path and node index.
+        @param json_file_path - the path to the JSON file
+        @param node_index - the index of the node
+        """
         print("*************BCM READ FROM JSON*************")
         conf = Conf.from_json(json_file_path, node_index)
 
@@ -96,15 +73,12 @@ class BCWorkloadState:
         Method used to tell this BCWorkloadStatus to read the filesystem and update itself
         accordingly.
 
-        In case of any errors, false is returned and empty data structures are filled in.
-
         The intended use case is scanning for the presence of new pod5 files to process, as
         well as reconstruct the internal state in case of a crash, based on what it finds in
         the filesystem.
 
-        :return: boolean with the result of the operation.
+        @return True if the update is successful, False if there are any errors.
         """
-
         # find all processed files i.e. get  list of all .FASTQ files
         fastq_files = []
         passoutputdir = os.path.join(self.OUTPUTDIR, "pass")
@@ -160,9 +134,10 @@ class BCWorkloadState:
         Finally, a zero or negative value of batch_size will automatically imply the use of the
         dorado_server's inbuilt default batch size.
 
-        :param dorado_server: a BCSvc
-        :param batch_size: optional int for the desired batch size
-        :return: BCBatch with the details of the assigned work
+        @param bc_engine_id - the ID of the Dorado server to assign work to
+        @param batch_size - optional integer specifying the desired batch size
+
+        @return BCBatch object containing details of the assigned work
         """
 
         # if there is nothing left to process, return an EMPTY BCBatch
@@ -195,6 +170,14 @@ class BCWorkloadState:
         return bc_work
 
     def completed_work(self, jobid="", jobstate=""):
+        """
+        Method invoked to complete the processing of a batch for a given job. It is based on its
+        state and updates the directories accordingly.
+
+        @param jobid - The ID of the job being completed.
+        @param jobstate - The state of the job (FAILED, DONE, or UNKNOWN).
+        
+        """
         # MAKE IT A TRANSACTION IN CASE OF CRASH
         bc_work = self.assigned_batches.pop(jobid)
         full_job_input_dir = os.path.join(self.INPUTDIR, bc_work.job_input_dir)
@@ -206,6 +189,7 @@ class BCWorkloadState:
             os.rename(full_job_input_dir, failed_full_job_input_dir)
             os.rename(full_job_output_dir, failed_full_job_output_dir)
         elif jobstate == bc_status.DONE:
+            #everything went all right
             passdir = os.path.join(full_job_output_dir,'pass')
             destpassdir = os.path.join(self.OUTPUTDIR, 'pass')
             if os.path.exists(passdir) & os.path.exists(destpassdir):
@@ -231,14 +215,23 @@ class BCWorkloadState:
             os.rename(full_job_output_dir, unknown_full_job_output_dir)
 
 
-
-
 class BCManager:
     """
-    Class that represents a RESTful Service listening for Basecalling work requests from Basecalling Engines.
+    A class representing a RESTful Service for managing Basecalling work requests from Basecalling Engines.
+
+    @param json_file_path - The path to the JSON file.
+    @param node_index - The index of the node.
+    @param shutdown_interval - The interval for shutting down.
     """
 
     def __init__(self,json_file_path, node_index, shutdown_interval=100):
+        """
+        Initialize the class with the provided parameters.
+
+        @param json_file_path - the path to the JSON file
+        @param node_index - the index of the node in the node list provided by the JSON
+        @param shutdown_interval - the interval for shutdown (default is 100)
+        """
         self.lock = threading.Lock()
         self.tracker = {} # dict of job_id -> [last_ack_time, state, report_back_period]
         self.bc_state = BCWorkloadState(json_file_path, node_index)
@@ -249,6 +242,15 @@ class BCManager:
         self.shutdown_interval = shutdown_interval
         self.last_activity_time = time.time()
 
+        """
+        Define a route "/assignwork" that handles GET requests to assign work to an engine. It takes in
+        the batch size and engine ID from the request parameters. It then assigns work to the specified
+        engine, updates the assignment report interval, and tracks the job status.
+         
+        Finally, it returns the assignment reply in JSON format.
+
+        @return JSON response with the assignment reply.
+        """
         # /assignwork
         @a.route("/assignwork", methods=["GET"])
         def get_assignwork():
@@ -261,6 +263,12 @@ class BCManager:
             self.update_last_activity_time()    #update activy time 
             return json.dumps(assignment_reply.__dict__)
 
+        """
+        Define a route "/keepalive" that handles GET requests to keep a job alive. It takes in the
+        job_id and the job_state from the request parameters.
+
+        @return A JSON response indicating whether the job is late or not.
+        """
         # /keepalive
         @a.route("/keepalive", methods=["GET"])
         def get_keepalive():
@@ -276,6 +284,13 @@ class BCManager:
             self.update_last_activity_time()    #update activy time 
             return json.dumps({"late": False})
 
+        """
+        Define a route that handles GET requests to retrieve completed job information. It 
+        takes in the job_id, The ID of the completed job, and the job_state. Then it modify 
+        the job_state by calling the method completed_work
+        
+        @return JSON response indicating the completion status.
+        """
         # /completed
         @a.route("/completed", methods=["GET"])
         def get_completed():
@@ -291,7 +306,13 @@ class BCManager:
             self.update_last_activity_time()    #update activy time 
             return json.dumps({"ok": True})    
         
-        # /heartbeat : for the observer that will close everything
+        """
+        Define a route '/heartbeat' that returns the status of the server based on the inactivity interval.
+        GET request to check the server's status.
+
+        @return JSON response with server status and inactivity interval.
+        """
+        # /heartbeat 
         @a.route('/heartbeat', methods=['GET'])
         def heartbeat():
             current_time = time.time()
@@ -301,9 +322,13 @@ class BCManager:
             else:
                 return jsonify({"status": "false", "inactivity_interval": inactivity_interval})
 
-    #Method that updates only when a /assignwork, /keepalive or /completed
-    #request are received
+    
     def update_last_activity_time(self):
+        """
+        Update the last_activity_time for when a /assignwork, /keepalive, /completed instance is called.
+        
+        @return None
+        """
         with self.lock:
             self.last_activity_time = time.time()            
             
@@ -318,50 +343,3 @@ if __name__ == '__main__':
 
 
 
-### NOTES:
-# what if the keepalive arrived late?
-# missing entry?
-# active deleting thread?
-# multiple requests for same jobid?
-# try:
-# MAKE A DEFENSIVE COPY OF THE WHOLE ENTRY: we have a deleting thread in the background that operates on it!
-#
-# AS PER BEST PRACTICE, EACH THREAD NEVER WORKS DIRECTLY IN THE ENTRY, BUT MAKES FIRST A DEFENSIVE COPY AND
-# THEN MODIFIES IT, BEFORE ASSIGNING IT BACK TO THE DICTIONARY. SO THE COPY IS SAFE AS NO DATA CHANGES.
-#
-# THIS ONLY GUARANTEES THE LOGICAL CONSISTENCY OF ANY DECISION/OPERATION MADE ON THE RETRIEVED DATA, WHICH IS
-# GUARANTEED FREE OF INTERLEAVING OF "PARTIAL CHUNKS" AS THE OTHER THREAD OPERATES ON THE DATA.
-#
-# IT DOES _NOT_ GUARANTEE ABSENCE OF RACE CONDITION / STALE READS! FOR THAT YOU STILL NEED SYNCHRONIZATION OF
-# SOME SORT.
-#
-# INDEED, THE RELEVANT SCENARIO IS WHEN WE GET A REFERENCE TO THE ENTRY, WHICH GETS REPLACED IMMEDIATELY AFTER
-# WE OBTAIN IT. WE'LL THEN GO ON TO MAKE A LOGICALLY CONSISTENT DECISION BASED ON THE DATA IN THE ENTRY, WHICH
-# IS NOW CLEARLY STALE/OLD. IN GENERAL WHETHER WE CAN TOLERATE OPERATION ON STALE DATA, OR WHETHER WE REALLY NEED
-# TO FORCE THE OTHER THREAD TO WAIT FOR US TO COMPLETE, WILL DEPEND ON THE ACTUAL PROBLEM DOMAIN. :-)
-#    entry = self.tracker[req_job_id].copy()
-#
-#    last_time = entry[0]
-#    report_back_period = entry[2]
-#    right_now = time.time()
-#    if right_now <= last_time + report_back_period:
-# OK: keep alive arrived before expiry
-#        entry[0] = right_now
-#        entry[1] = req_job_state
-# entry[2] stays the same
-#        self.tracker[req_job_id] = entry
-#        return json.dumps({"late": False})
-#    else:
-# BAD: keepalive arrived late
-#        print("WARNING! NOT IMPLEMENTED YET! keepalive arrived too late: client should shutdown, and work be re-assigned!")
-# remove from tracking ?
-# clear state so old work can be reassigned?
-# tell client it was late?
-# interaction with deleting thread and other threads?
-#        return json.dumps({"late": True})
-# except KeyError:
-# ENTRY DOES NOT EXIST! EITHER IT WAS DELETED BY THE DELETING THREAD BECAUSE IT WAS EXPIRED, OR IT NEVER EXISTED.
-# INFORM CLIENT! AS PER PROTOCOL, IT SHOULD STOP PROCESSING AND INITIATE A SHUTDOWN.
-#    print("WARNING! NOT IMPLEMENTED YET! keepalive arrived for non-existant jobid!")
-# tell client?
-#    return json.dumps({"late": True})
--- docs/BC_software.md
+++ docs/BC_software.md
@@ -8,4 +8,91 @@ This directory contains all the
 
 ## BCManager
 
+### BCWorkloadState:
+Class that represents the state of the basecalling processing, as well as
+providing methods for operating on it.  
+
+Essentially all the raw output from the Oxford Nano Pore sequencing machine
+consists of .POD5 files that the machine writes to a specific directory we'll
+refer to as INPUT_DIR.  
+
+Each raw file, is the recording of the electrical signal corresponding to the
+particular DNA chunk that was sequenced. The chunks are known as frags, while the
+recordings are also called squibbles because of the shape of the resulting graph.
+
+Basically this raw data represents electrical current/potential over time.  
+For each file, a corresponding .FASTQ file must be produced and stored in
+OUTPUT_DIR.  
+
+This is the basecalled file: a very long string of 4 letters representing the
+biological bases, G A T C, that translates the raw electrical signals.  
+The actual basecalling functionality is carried out by a different software/system,
+i.e. the DORADO system. But there are also other systems such as BONITO that can be used.
+
+This software is needed because the algorithms involved in basecalling make use of
+neural networks in order to translate the electrical signals, and so are rather
+specialised for the task requiring also the availability of GPUs.  
+
+Several instances of the DORADO servers are expected to be available: at least one for each
+GPU in the infrastructure. Moreover, each DORADO server is designed to work on multiple files
+which it expects to be present in a specified directory.  
+
+The class allows raw files to be assigned to one of the DORADO server for processing. This
+will be reflected in the filesystem by the presence of the following structure:
+
+INPUT_DIR
+    |- JOB-ID1_DORADO-SERVER-NAME_inputdir
+        |- file1.pod5   (ATTENTION! IT WILL BE A SYMLINK!)
+        |- file2.pod5   (ATTENTION! IT WILL BE A SYMLINK!)
+        |- file3.pod5   (ATTENTION! IT WILL BE A SYMLINK!)
+
+#### NOTES:
+what if the keepalive arrived late?  
+missing entry?  
+active deleting thread?  
+multiple requests for same jobid?  
+try:  
+MAKE A DEFENSIVE COPY OF THE WHOLE ENTRY: we have a deleting thread in the background that operates on it!  
+
+AS PER BEST PRACTICE, EACH THREAD NEVER WORKS DIRECTLY IN THE ENTRY, BUT MAKES FIRST A DEFENSIVE COPY AND THEN MODIFIES IT, BEFORE ASSIGNING IT BACK TO THE DICTIONARY. SO THE COPY IS SAFE AS NO DATA CHANGES.  
+
+THIS ONLY GUARANTEES THE LOGICAL CONSISTENCY OF ANY DECISION/OPERATION MADE ON THE RETRIEVED DATA, WHICH IS GUARANTEED FREE OF INTERLEAVING OF "PARTIAL CHUNKS" AS THE OTHER THREAD OPERATES ON THE DATA.  
+
+IT DOES _NOT_ GUARANTEE ABSENCE OF RACE CONDITION / STALE READS! FOR THAT YOU STILL NEED SYNCHRONIZATION OF SOME SORT.    
+
+INDEED, THE RELEVANT SCENARIO IS WHEN WE GET A REFERENCE TO THE ENTRY, WHICH GETS REPLACED IMMEDIATELY AFTER WE OBTAIN IT. WE'LL THEN GO ON TO MAKE A LOGICALLY CONSISTENT DECISION BASED ON THE DATA IN THE ENTRY, WHICH IS NOW CLEARLY STALE/OLD.  
+IN GENERAL WHETHER WE CAN TOLERATE OPERATION ON STALE DATA, OR WHETHER WE REALLY NEED TO FORCE THE OTHER THREAD TO WAIT FOR US TO COMPLETE, WILL DEPEND ON THE ACTUAL PROBLEM DOMAIN. 
+```python
+# entry = self.tracker[req_job_id].copy()
+last_time = entry[0]
+report_back_period = entry[2]
+right_now = time.time()
+
+if right_now <= last_time + report_back_period:
+    # OK: keep alive arrived before expiry
+    entry[0] = right_now
+    entry[1] = req_job_state
+    # entry[2] stays the same
+    self.tracker[req_job_id] = entry
+    return json.dumps({"late": False})
+else:
+    # BAD: keepalive arrived late
+    print("WARNING! NOT IMPLEMENTED YET! keepalive arrived too late: client should shutdown, and work be re-assigned!")
+    # remove from tracking ?
+    # clear state so old work can be reassigned?
+    # tell client it was late?
+    # interaction with deleting thread and other threads?
+    return json.dumps({"late": True})
+
+except KeyError:
+    # ENTRY DOES NOT EXIST! EITHER IT WAS DELETED BY THE DELETING THREAD BECAUSE IT WAS EXPIRED, OR IT NEVER EXISTED.
+    # INFORM CLIENT! AS PER PROTOCOL, IT SHOULD STOP PROCESSING AND INITIATE A SHUTDOWN.
+    print("WARNING! NOT IMPLEMENTED YET! keepalive arrived for non-existent jobid!")
+    # tell client?
+    return json.dumps({"late": True})
+```
+
+
 ## BCController
+ASSIGNED_(jobid)_(bc_engine_id)         
+LOGOUTPUT_(jobid)_(bc_engine_id)
