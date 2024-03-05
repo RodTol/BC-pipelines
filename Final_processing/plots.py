38355a8d292645668e9d0cf3d1a9f531e793be32
RodTol <rodolfo.tolloi@gmail.com>
2024-03-05 16:56:08 +0100

    plots.py improved bar_chart

--- Final_processing/plots.py
+++ Final_processing/plots.py
@@ -16,12 +16,29 @@ plt.text(0, -1.2, f'Total Files: {total_files}', ha='center', va='center', fonts
 plt.savefig('pie_chart.png')
 plt.close()
 
+# Get unique nodes and assign unique colors
+unique_nodes = df['Node'].unique()
+colors = plt.cm.viridis(range(len(unique_nodes)))
+
+# Create a color dictionary mapping each node to its color
+node_color_dict = dict(zip(unique_nodes, colors))
+
 # Plot bar chart
-plt.figure(figsize=(10, 6))
-plt.barh(df["Node"], df["Samples/s"], color='skyblue')
+plt.figure(figsize=(12, 8))
+
+for index, row in df.iterrows():
+    node = row['Node']
+    color = node_color_dict.get(node, 'gray')  # Use gray if the node is not explicitly defined in node_color_dict
+    plt.barh(row['Node'], row['Samples/s'], color=color, label=f'{row["Input Read Files"]}', alpha=0.7)
+
+# Customize plot
 plt.xlabel('Samples/s')
 plt.ylabel('Node')
-plt.title('Samples/s Performance of Each Node')
+plt.title('Samples/s Performance of Each Run')
+plt.legend(title='Input Read Files', loc='upper right', bbox_to_anchor=(1.15, 1))
+plt.grid(axis='x', linestyle='--', alpha=0.7)
+
+plt.tight_layout()
 plt.savefig('bar_chart.png')
 plt.close()
 
