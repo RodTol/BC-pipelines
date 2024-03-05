786ebe36b17381a30e697d530a6a5d948a00c0f0
RodTol <rodolfo.tolloi@gmail.com>
2024-03-05 17:30:34 +0100

    barc chart restart

--- Final_processing/plots.py
+++ Final_processing/plots.py
@@ -18,16 +18,15 @@ plt.savefig('pie_chart.png')
 plt.close()
 
 # Group by Node and create a bar plot for each run
-grouped_df = df.groupby('Node')
+# Group by Node and plot a bar for each node
 fig, ax = plt.subplots()
-
-for name, group in grouped_df:
-    ax.bar(group['Input Read Files'], group['Samples/s'], label=name)
+for name, group in df.groupby('Node'):
+    ax.bar(name, group['Samples/s'].values[0], label=name)
 
 # Set labels and title
-ax.set_xlabel('Input Read Files')
+ax.set_xlabel('Node')
 ax.set_ylabel('Samples/s')
-ax.set_title('Samples/s for Each Node and Run')
+ax.set_title('Samples/s for Each Node')
 ax.legend(title='Node')
 
 plt.savefig('bar_chart.png')
