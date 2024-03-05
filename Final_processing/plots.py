844a8e43a24a6c097ca3b25b5c33ba6fef1d6a08
RodTol <rodolfo.tolloi@gmail.com>
2024-03-05 16:49:45 +0100

    plots.py new bar_chart plot

--- Final_processing/plots.py
+++ Final_processing/plots.py
@@ -12,13 +12,21 @@ total_files = file_counts.sum()
 plt.figure(figsize=(10, 10))
 plt.pie(file_counts, labels=file_counts.index, autopct='%1.0f%%', startangle=140)
 
-# Display the number of files instead of percentages
-plt.gca().set_aspect('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
 plt.title('Number of Files Processed by Each Node')
 
 # Display the total number of files
 plt.text(0, -1.2, f'Total Files: {total_files}', ha='center', va='center', fontsize=12, color='darkblue')
 
 plt.savefig('pie_chart.png')
+#---------------------------
+
+plt.barh(df["Node"], df["Samples/s"], color='skyblue')
+plt.xlabel('Samples/s')
+plt.ylabel('Node')
+
+plt.title('Samples/s Performance of Each Node')
+
+plt.savefig('bar_chart.png')
 
 
