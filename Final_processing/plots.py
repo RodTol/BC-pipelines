f678f74f0518e4a2d6f999c058e49351bfa9a59e
RodTol <rodolfo.tolloi@gmail.com>
2024-03-05 16:22:51 +0100

    plots.py 1

---
+++ Final_processing/plots.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+
+csv_file_path = sys.argv[1]
+df = pd.read_csv(csv_file_path)
+
+# Group data by Node and sum the number of files processed
+file_counts = df.groupby('Node')['Input Read Files'].sum()
+
+# Create a pie chart
+plt.figure(figsize=(8, 8))
+plt.pie(file_counts, labels=file_counts.index, autopct='%1.1f%%', startangle=140)
+plt.title('Number of Files Processed by Each Node')
+plt.show()
