3c967a616361109850b4fb7f8f8c54f455619c88
RodTol <rodolfo.tolloi@gmail.com>
2024-03-05 17:07:29 +0100

    plots.py improved bar_chart 3

--- Final_processing/plots.py
+++ Final_processing/plots.py
@@ -25,15 +25,24 @@ fig, ax = plt.subplots()
 colors = iter(plt.cm.rainbow_r(np.linspace(0, 1, len(grouped_data))))
 
 # Plot each group separately
-for name, group in grouped_data:
+bar_width = 0.8 / len(grouped_data)  # Adjusted width for thinner bars
+for i, (name, group) in enumerate(grouped_data):
     color = next(colors)
-    ax.bar(group.index, group['Samples/s'], color=color, label=f'{name} - {group["Input Read Files"].sum()} files')
+    x_values = np.arange(len(group)) + i * bar_width
+    ax.bar(x_values, group['Samples/s'], width=bar_width, color=color, label=name)
+    
+    # Add text label near each column with the number of input files
+    for j, value in enumerate(group['Input Read Files']):
+        ax.text(x_values[j], group['Samples/s'].iloc[j], str(value),
+                ha='center', va='bottom', fontsize=8, color='black')
 
 # Set labels and title
-ax.set_xlabel('Run Index')
+ax.set_xlabel('Node')
 ax.set_ylabel('Samples/s')
 ax.set_title('Run Speed Comparison')
-ax.legend()
+ax.set_xticks(np.arange(len(df['Node'].unique())) + (bar_width * (len(grouped_data) - 1)) / 2)
+ax.set_xticklabels(df['Node'].unique())
+plt.legend(title='Node', loc='upper right')
 plt.savefig('bar_chart.png')
 plt.close()
 
