68885fd2ec2d1f1b9e7437eef0056c992d00b183
RodTol <rodolfo.tolloi@gmail.com>
2024-03-06 15:13:20 +0100

    new try for html parsing

--- BC_scripts/startup.sh
+++ BC_scripts/startup.sh
@@ -3,9 +3,15 @@
 # Function to send a message to Telegram
 send_message() {
  local message="$1"
+ 
+ local background_color="$2"
+ 
+ local formatted_message="<div style=\"background-color:${background_color}; padding: 10px;\">${message}</div>"
+ 
  curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
  -d "chat_id=$CHAT_ID" \
- -d "text=$message"
+ -d "text=$formatted_message" \
+ -d "parse_mode=HTML"
 }
 
 BOT_TOKEN="$BC_TOKEN_BOT"
@@ -16,5 +22,5 @@ run_name=$(jq -r '.General.run_name' "$json_file")
 model=$(jq -r '.Basecalling.model' "$json_file")
 nodes_list=$(jq -r '.Resources.nodes_list' "$json_file")
 
-send_message "The $run_name is started at $(date +"%H:%M:%S")." > /dev/null
-send_message "The basecalling will be executed on $nodes_list with model $model"  > /dev/null
+send_message "The $run_name is started at $(date +"%H:%M:%S")." "#BB8FCE" > /dev/null
+send_message "The basecalling will be executed on $nodes_list with model $model" "#BB8FCE" > /dev/null
