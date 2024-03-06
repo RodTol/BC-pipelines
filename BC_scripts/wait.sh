b514f9a0ac46b8fa41e9e58c95fff8cd4a5c3ce3
RodTol <rodolfo.tolloi@gmail.com>
2024-03-06 16:58:30 +0100

    mark down message 2

--- BC_scripts/wait.sh
+++ BC_scripts/wait.sh
@@ -10,9 +10,9 @@ check_job_status() {
 # Function to send a message to Telegram with code block formatting
 send_message() {
  local message="$1"
- local formatted_message="\\\`\\\`\\\`bash
+ local formatted_message="```
 $message
-\\\`\\\`\\\`"
+```"
 
  curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
  -d "chat_id=$CHAT_ID" \
