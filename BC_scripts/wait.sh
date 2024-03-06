b29af19ee868f5532fdab0520b65e84235658a20
RodTol <rodolfo.tolloi@gmail.com>
2024-03-06 17:01:04 +0100

    fixed message function

--- BC_scripts/wait.sh
+++ BC_scripts/wait.sh
@@ -28,7 +28,7 @@ CHAT_ID="-4074077922"
 start_time=$(date +%s)
 interval=200 
 
-send_message "Job $JOB_ID is queued at $(date +"%H:%M:%S")" "#F1948A"  
+send_message "Job $JOB_ID is queued at $(date +"%H:%M:%S")"
 
 # Loop until the job is completed
 while check_job_status; do
@@ -40,13 +40,13 @@ while check_job_status; do
     if ((elapsed_time >= interval)); then
         JOB_STATUS=$(scontrol show job $JOB_ID | awk '/JobState=/{print $1}')
         echo "Slurm job $JOB_ID status: $JOB_STATUS"
-        send_message "At $(date +"%H:%M:%S") Slurm job $JOB_ID status is $JOB_STATUS" "#F1948A" > /dev/null
+        send_message "At $(date +"%H:%M:%S") Slurm job $JOB_ID status is $JOB_STATUS" > /dev/null
         
         # Capture the output of squeue -p DGX,GPU
         squeue_output=$(squeue -p DGX,GPU)
 
         # Send the captured output as a message
-        send_message "$squeue_output" "#F1948A" > /dev/null
+        send_message "$squeue_output" > /dev/null
 
         # Reset the start time
         start_time=$current_time
