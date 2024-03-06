e91f8393772b05b64b86a85d4224f579596d2ee3
RodTol <rodolfo.tolloi@gmail.com>
2024-03-06 14:24:22 +0100

    added startup | modified wait for more interactive update | correction on the pipeline

---
+++ BC_scripts/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Function to send a message to Telegram
+send_message() {
+ local message="$1"
+ curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+ -d "chat_id=$CHAT_ID" \
+ -d "text=$message"
+}
+
+BOT_TOKEN="$BC_TOKEN_BOT"
+CHAT_ID="-4074077922"
+
+json_file=$1
+run_name=$(jq -r '.General.run_name' "$json_file")
+model=$(jq -r '.Basecalling.model' "$json_file")
+nodes_list=$(jq -r '.Resources.nodes_list' "$json_file")
+
+send_message "The $run_name is started at $(date +%s)."
+send_message "The basecalling will be executed on $nodes_list with model $model" 
--- BC_scripts/wait.sh
+++ BC_scripts/wait.sh
@@ -7,11 +7,44 @@ check_job_status() {
     scontrol show job $JOB_ID &> /dev/null
 }
 
+# Function to send a message to Telegram
+send_message() {
+ local message="$1"
+ curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+ -d "chat_id=$CHAT_ID" \
+ -d "text=$message"
+}
+
+BOT_TOKEN="$BC_TOKEN_BOT"
+CHAT_ID="-4074077922"
+
+# Set initial time and interval
+start_time=$(date +%s)
+interval=300 
+
+send_message "Job $JOB_ID is queued at $start_time"
+
 # Loop until the job is completed
 while check_job_status; do
-    # Print the current status of the job
-    JOB_STATUS=$(scontrol show job $JOB_ID | awk '/JobState=/{print $1}')
-    echo "Slurm job $JOB_ID status: $JOB_STATUS"
+
+    # Print the current status of the job every interval
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+
+    if ((elapsed_time >= interval)); then
+        JOB_STATUS=$(scontrol show job $JOB_ID | awk '/JobState=/{print $1}')
+        echo "Slurm job $JOB_ID status: $JOB_STATUS"
+        send_message "At $current_time Slurm job $JOB_ID status is $JOB_STATUS"
+        
+        # Capture the output of squeue -p DGX,GPU
+        squeue_output=$(squeue -p DGX,GPU)
+
+        # Send the captured output as a message
+        send_message "$squeue_output"
+
+        # Reset the start time
+        start_time=$current_time
+    fi
 
     # If job is Finished, Cancelled or Failed, jenkins will stop waiting
     if [[ "$JOB_STATUS" == "JobState=FAILED" || "$JOB_STATUS" == "JobState=CANCELLED" || "$JOB_STATUS" == "JobState=COMPLETED" ]]; then
