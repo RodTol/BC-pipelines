b336a5a2b9754e9ffd1654b1ef1ee396cb8f27eb
RodTol <rodolfo.tolloi@gmail.com>
2024-03-22 16:58:45 +0100

    data_flow_emulato 3.0

--- utility/data_flow_emulator.py
+++ utility/data_flow_emulator.py
@@ -2,7 +2,7 @@ import os
 import sys
 import shutil
 import time
-from alive_progress import alive_bar
+from alive_progress import alive_it
 
 def mimic_live_writing(src_dir, dest_dir, interval_seconds=10):
     files = os.listdir(src_dir)
@@ -25,21 +25,20 @@ def mimic_live_writing_groups(src_dir, dest_dir, interval_seconds=10, n_files=4)
     files_set = set(files)  # Set to keep track of copied files
     
     counter = 0
-    with alive_bar(len(files)) as bar:   # Expected total
-        for file in files_set:   
-            counter = counter+1 
-            src_file_path = os.path.join(src_dir, file)
-            dest_file_path = os.path.join(dest_dir, file)
 
-            print(f"Copying file: {file}", end=" ")
-            shutil.copy(src_file_path, dest_file_path)
-            print("Copy successful")
-            bar()
-            
-            if (counter==n_files):
-                counter=0
-                time.sleep(interval_seconds)
-        
+    for file in alive_it(files_set):   
+        counter = counter+1 
+        src_file_path = os.path.join(src_dir, file)
+        dest_file_path = os.path.join(dest_dir, file)
+
+        print(f"Copying file: {file}", end=" ")
+        shutil.copy(src_file_path, dest_file_path)
+        print("Copy successful")
+
+        if (counter==n_files):
+            counter=0
+            time.sleep(interval_seconds)
+    
     print("No new files to copy.")
 
 if __name__ == "__main__":
