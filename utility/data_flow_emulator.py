7234384fb98cb353d2c797bf730a55aa1d064a4f
RodTol <rodolfo.tolloi@gmail.com>
2024-03-22 16:20:21 +0100

    data_flow_emulato progress bar 2

--- utility/data_flow_emulator.py
+++ utility/data_flow_emulator.py
@@ -2,23 +2,25 @@ import os
 import sys
 import shutil
 import time
-from tqdm import tqdm
+from alive_progress import alive_bar
 
-def mimic_live_writing(src_dir, dest_dir, interval_seconds=30):
+def mimic_live_writing(src_dir, dest_dir, interval_seconds=10):
     files = os.listdir(src_dir)
     files_set = set(files)  # Set to keep track of copied files
 
-    for file in tqdm(files_set):    
-        src_file_path = os.path.join(src_dir, file)
-        dest_file_path = os.path.join(dest_dir, file)
-        print(f"Copying file: {file}", end=" ")
-        shutil.copy(src_file_path, dest_file_path)
-        print("Copy successful")
-    
-        time.sleep(interval_seconds)
-    
+    with alive_bar(len(files)) as bar # Expected total
+        for file in files_set:    
+            src_file_path = os.path.join(src_dir, file)
+            dest_file_path = os.path.join(dest_dir, file)
+
+            print(f"Copying file: {file}", end=" ")
+            shutil.copy(src_file_path, dest_file_path)
+            print("Copy successful")
+
+            time.sleep(interval_seconds)
+            bar()
+        
     print("No new files to copy.")
-    sys.exit()        
 
 if __name__ == "__main__":
     src_dir = sys.argv[1]  # Source directory
