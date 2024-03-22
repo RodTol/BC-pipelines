53095303c8b3cd24276347186dd5338f39b69af2
RodTol <rodolfo.tolloi@gmail.com>
2024-03-22 16:37:13 +0100

    data_flow_emulato progress bar 4

--- utility/data_flow_emulator.py
+++ utility/data_flow_emulator.py
@@ -17,9 +17,27 @@ def mimic_live_writing(src_dir, dest_dir, interval_seconds=10):
             shutil.copy(src_file_path, dest_file_path)
             print("Copy successful")
 
-            bar()
             time.sleep(interval_seconds)
+            bar()
 
+def mimic_live_writing_groups(src_dir, dest_dir, interval_seconds=10, n_files=4):
+    files = os.listdir(src_dir)
+    files_set = set(files)  # Set to keep track of copied files
+
+    with alive_bar(len(files)) as bar:   # Expected total
+        for file in files_set:   
+            counter = counter+1 
+            src_file_path = os.path.join(src_dir, file)
+            dest_file_path = os.path.join(dest_dir, file)
+
+            print(f"Copying file: {file}", end=" ")
+            shutil.copy(src_file_path, dest_file_path)
+            print("Copy successful")
+
+            if (counter==n_files):
+                counter=0
+                time.sleep(interval_seconds)
+                bar()
         
     print("No new files to copy.")
 
