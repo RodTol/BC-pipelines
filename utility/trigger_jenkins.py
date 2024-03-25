6ffaadf1e1952768a8c5dd4cd1b22191fcdeabfa
RodTol <rodolfo.tolloi@gmail.com>
2024-03-25 12:15:49 +0100

    python trigger 2

--- utility/trigger_jenkins.py
+++ utility/trigger_jenkins.py
@@ -3,7 +3,7 @@ import sys
 
 def trigger_jenkins_pipeline(jenkins_url, job_name, parameters, api_token):
     # Jenkins job URL
-    job_url = f"{jenkins_url}/job/{job_name}//buildWithParameters?token={api_token}"
+    job_url = f"{jenkins_url}/job/{job_name}/buildWithParameters?token={api_token}"
 
     try:
         # Send POST request to trigger the Jenkins pipeline with parameters
@@ -18,10 +18,10 @@ if __name__ == "__main__":
     api_token = sys.argv[1]  # Source directory
 
     # Jenkins URL
-    jenkins_url = "http://jenkins-sandbox.rd.areasciencepark.it:8080/"  # Replace with your Jenkins server URL
+    jenkins_url = "http://jenkins-sandbox.rd.areasciencepark.it:8080"  # Replace with your Jenkins server URL
 
     # Jenkins job name
-    job_name = "tolloi/job/basecalling_pipeline/"  # Replace with your Jenkins pipeline job name
+    job_name = "tolloi/job/basecalling_pipeline"  # Replace with your Jenkins pipeline job name
 
     # Parameters for the Jenkins pipeline
     config_path = {
