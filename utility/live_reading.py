06579d405ece3d53dbd949d113f42c71bb2e8997
RodTol <rodolfo.tolloi@gmail.com>
2024-04-22 16:57:49 +0200

    CLiveOME run fixed

--- utility/live_reading.py
+++ utility/live_reading.py
@@ -190,7 +190,7 @@ class Live_Reading :
 
         return jenkins_parameter
 
-    def live_reading_dir(self, threshold=5, scanning_time=5, max_retry=5):
+    def live_reading_dir(self, threshold=12, scanning_time=5, max_retry=5):
         '''
         The purpouse of this function is to scan the input directory and trigger
         the basecalling pipeline when we have added more than "threshold" files.
