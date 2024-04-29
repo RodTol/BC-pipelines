2344af2efecbf3357c064a346251879301fcd829
RodTol <rodolfo.tolloi@gmail.com>
2024-04-29 15:29:27 +0200

    added batch size to the message

--- BC_scripts/startup.sh
+++ BC_scripts/startup.sh
@@ -10,6 +10,12 @@ send_message() {
  -d "text=$message"
 }
 
+count_symbolic_links() {
+    local directory="$1"
+    local symlink_count=$(find "$directory" -type l | wc -l)
+    echo "$symlink_count"
+}
+
 BOT_TOKEN="$BC_TOKEN_BOT"
 CHAT_ID="-4074077922"
 
@@ -18,6 +24,9 @@ run_name=$(jq -r '.General.run_name' "$json_file")
 model=$(jq -r '.Basecalling.model' "$json_file")
 nodes_list=$(jq -r '.Resources.nodes_list' "$json_file")
 
-message="<b> Jenkins is building for $run_name is at $(date +"%H:%M:%S")."
+input_dir=$(jq -r '.Basecalling.input_dir' "$json_file")
+symlink_count=$(count_symbolic_links $input_dir)
+
+message="<b> Jenkins is building for $run_name is at $(date +"%H:%M:%S"). This run will process $symlink_count files, located at $input_dir"
 
 send_message $message > /dev/null
