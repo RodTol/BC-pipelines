15c5724f3d569523fad0a539e70425eb5f3dfc59
RodTol <rodolfo.tolloi@gmail.com>
2024-04-30 16:20:30 +0200

    BCM plot

--- Final_processing/BCM_parsing.py
+++ Final_processing/BCM_parsing.py
@@ -5,6 +5,34 @@ import os
 import pandas as pd
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+import hashlib
+import numpy as np
+
+def generate_colors(ips):
+    colors = []
+    np.random.seed(1)  # Set a fixed seed for reproducibility
+    for ip in ips:
+        # Convert the IP address to a unique hash
+        hash_object = hashlib.md5(ip.encode())
+        hash_digest = hash_object.hexdigest()
+        
+        # Take the first 6 characters of the hash to represent the color
+        color_hex = '#' + hash_digest[:6]
+        
+        # Convert hex color to RGB
+        r = int(color_hex[1:3], 16) / 255.0
+        g = int(color_hex[3:5], 16) / 255.0
+        b = int(color_hex[5:7], 16) / 255.0
+        
+        # Normalize RGB values to fit into Viridis colormap
+        color = np.array([r, g, b]).reshape(1, -1)
+        viridis_color = plt.cm.viridis(color)[0, :3]  # Get RGB values from Viridis colormap
+        
+        colors.append(viridis_color)
+    
+    return colors
+
 # Function to load JSON data from a file
 def load_json(json_file):
     with open(json_file, 'r') as file:
@@ -75,7 +103,7 @@ def find_for_job_id_times(log_file, job_id):
     time2 = datetime.strptime(end_time, time_format)
 
     # Calculate the time difference
-    elapsed_time = time2 - time1
+    elapsed_time = (time2 - time1).total_seconds()
 
     times = [start_time, end_time, elapsed_time]
 
@@ -103,6 +131,33 @@ def insert_times_in_df(log_file_path, df):
     df['elapsed_times'] = elapsed_times
 
     return None
+
+
+def plot(df):
+
+    colors = generate_colors(df['ips'])
+    x = [0]
+    for i, row in df.iterrows():
+        x.append((len(row['job_ids'])+1)*0.25)
+    # Plot each row
+    for i, row in df.iterrows():
+        for j, elapsed_time in enumerate(row['elapsed_times']):
+            if j==0:
+                plt.bar(x[i] + j*0.25, elapsed_time, label=row['ips'], color=colors[i], width=0.2, align='center')
+            else:
+                plt.bar(x[i] + j*0.25, elapsed_time, color=colors[i], width=0.2, align='center')
+    plt.ylabel('Elapsed Time')
+    plt.xlabel('IPs')
+    plt.title('Elapsed Time by IP Address')
+
+    # Set x-axis ticks and labels to empty list
+    plt.xticks([], [])
+
+    # Create legend
+    plt.legend()
+
+    # Save the plot to a file
+    plt.savefig('plot_times.png')
 
 if __name__ == "__main__":
     # Check if the correct number of command-line arguments is provided
@@ -132,4 +187,10 @@ if __name__ == "__main__":
     table["job_ids"] = job_ids
 
     insert_times_in_df(log_file_path, table)
-    print(table)
+
+    #print(table)
+    # Create a CSV file to store the extracted information
+    csv_output = 'times_' + config['General']['run_name'] + '.csv'
+    table.to_csv(csv_output, index=False)
+
+    plot(table)
