53f3fbbce2728ab4b64f90b07a925a09f29f5a81
RodTol <rodolfo.tolloi@gmail.com>
2024-05-07 11:17:49 +0200

    back to epyc

--- simulation-pipeline/scripts/script_live_scanner.sh
+++ simulation-pipeline/scripts/script_live_scanner.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=/u/area/jenkins_onpexp/scratch/jenkins_logs/tmp/live_scanner.out
 #SBATCH --error=/u/area/jenkins_onpexp/scratch/jenkins_logs/tmp/live_scanner.err
 #SBATCH -A lage
-#SBATCH -p DGX
+#SBATCH -p EPYC
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
@@ -13,5 +13,5 @@
 # The $1 argument is the path to the template_config.json file
 # The $2 is the path to the input directory that will be studied
 
-source /u/area/jenkins_onpexp/python_venvs/dgx_venv_jenkins/bin/activate
+source /u/area/jenkins_onpexp/python_venvs/epyc_venv_jenkins/bin/activate
 python3 /u/area/jenkins_onpexp/BC-pipelines/simulation-pipeline/utility/main.py Alfredo95 elefante $1 $2
