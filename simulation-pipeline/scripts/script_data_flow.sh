048749fe2cee1e49bbb27a1d28c7c97a108fd588
RodTol <rodolfo.tolloi@gmail.com>
2024-05-08 12:52:40 +0200

    run on DGX

--- simulation-pipeline/scripts/script_data_flow.sh
+++ simulation-pipeline/scripts/script_data_flow.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=/u/area/jenkins_onpexp/scratch/jenkins_logs/tmp/data_flow.out
 #SBATCH --error=/u/area/jenkins_onpexp/scratch/jenkins_logs/tmp/data_flow.err
 #SBATCH -A lage
-#SBATCH -p EPYC
+#SBATCH -p DGX
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
@@ -13,5 +13,6 @@
 source=$1
 dest=$2
 
-source /u/area/jenkins_onpexp/python_venvs/epyc_venv_jenkins/bin/activate
+#source /u/area/jenkins_onpexp/python_venvs/epyc_venv_jenkins/bin/activate
+source /u/area/jenkins_onpexp/python_venvs/dgx_venv_jenkins/bin/activate
 python3 /u/area/jenkins_onpexp/BC-pipelines/simulation-pipeline/utility/data_flow_emulator.py $source $dest
