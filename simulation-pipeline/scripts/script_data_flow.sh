73c8e1b3b0cbc7d1f70d98fab8b60692b4b3a6db
RodTol <rodolfo.tolloi@gmail.com>
2024-05-09 16:28:21 +0200

    run on DGX

--- simulation-pipeline/scripts/script_data_flow.sh
+++ simulation-pipeline/scripts/script_data_flow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=data_flow
-#SBATCH --time=03:00:00
+#SBATCH --time=06:00:00
 #SBATCH --output=/u/area/jenkins_onpexp/scratch/jenkins_logs/tmp/data_flow.out
 #SBATCH --error=/u/area/jenkins_onpexp/scratch/jenkins_logs/tmp/data_flow.err
 #SBATCH -A lage
@@ -15,4 +15,5 @@ dest=$2
 
 #source /u/area/jenkins_onpexp/python_venvs/epyc_venv_jenkins/bin/activate
 source /u/area/jenkins_onpexp/python_venvs/dgx_venv_jenkins/bin/activate
+
 python3 /u/area/jenkins_onpexp/BC-pipelines/simulation-pipeline/utility/data_flow_emulator.py $source $dest
