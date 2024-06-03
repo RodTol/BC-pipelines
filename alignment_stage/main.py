3a4e02f47aacd17c27a73ab08c7e9e0d7a14ecb3
RodTol <rodolfo.tolloi@gmail.com>
2024-06-03 15:20:32 +0200

    feat: :art: First implementation of the creation of .json file for the alignment
    Added creation at end of BC-pipeliene of a temporary config file that wll be used  by the subsequent AL-pipeline

--- alignment_stage/main.py
+++ alignment_stage/main.py
@@ -1,7 +1,8 @@
 import sys
-from merge_and_config import merge_fastq
+from merge_and_config import *
 
 if __name__ == "__main__":
 
     config_file_path = sys.argv[1]
     merge_fastq(config_file_path)
+    create_configurations_file(config_file_path)
--- alignment_stage/merge_and_config.py
+++ alignment_stage/merge_and_config.py
@@ -43,20 +43,51 @@ def create_configurations_file(job_config_path) :
     selected output directory
     '''
     with open(job_config_path, 'r') as file:
-        config = json.load(file)
+        BC_config = json.load(file)
 
-    config_dir = config["Alignment"]["path_to_tmp_config"]
-    run_number = re.search(r'\d+', config["General"]["run_name"]).group()
+    config_dir = BC_config["Alignment"]["path_to_tmp_config"]
+    run_number = re.search(r'\d+', BC_config["General"]["run_name"]).group()
     tmp_config_name = f'align_config_{str(run_number)}.json'
 
     #Create a copy of the template for the alignment in the correct dir
-    path_to_template_config = config["Alignment"]["template_config"]
-    path_for_tmp_config = os.path.join(config_dir, tmp_config_name)
+    path_to_al_template_config = BC_config["Alignment"]["template_config"]
+    path_for_al_tmp_config = os.path.join(config_dir, tmp_config_name)
 
-    shutil.copy(path_to_template_config, path_for_tmp_config)
+    shutil.copy(path_to_al_template_config, path_for_al_tmp_config)
 
+    #Open the tmp_config 
+    with open(path_for_al_tmp_config, 'r') as file:
+        al_tmp_config = json.load(file)
+
+    #Name
+    al_tmp_config['General']['run_name'] =  "AL_"+ BC_config['General']['run_name']
     
-    
+    # Slurm log dir is the same of BC but different name
+    # Split the path into directory and file name
+    log_dir_path, log_file_name = os.path.split(BC_config["Slurm"]["output"])
+    modified_log_file_name = "AL" + log_file_name
+    modified_path = os.path.join(log_dir_path, modified_log_file_name)
+    al_tmp_config['Slurm']['output'] = modified_path
+
+    log_dir_path, log_file_name = os.path.split(BC_config["Slurm"]["error"])
+    modified_log_file_name = "AL" + log_file_name
+    modified_path = os.path.join(log_dir_path, modified_log_file_name)
+    al_tmp_config['Slurm']['error'] = modified_path
+
+    #Input file
+    pass_merged_file = os.path.join(BC_config['Basecalling']['output_dir'], "pass_batch_" + run_number + "_total.fastq")
+    al_tmp_config["Alignment"]["input_file"] = pass_merged_file
+
+    #Output file
+    sam_file_name = "Batch_" + run_number + ".sam"
+    output_file = os.path.join(BC_config["Basecalling"]["output_dir"], sam_file_name)
+    al_tmp_config["Alignment"]["output_file"] = output_file
+
+    # Convert the modified data structure back to JSON format
+    json_content = json.dumps(al_tmp_config, indent=2)
+    # Write the modified JSON content back to the copied file
+    with open(path_for_al_tmp_config, 'w') as file:
+        file.write(json_content)    
     
 
 
