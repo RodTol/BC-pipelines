76a8044d17e6f1e6d18ca824841efd8ebb61e6c1
RodTol <rodolfo.tolloi@gmail.com>
2024-06-03 12:20:20 +0200

    fix for message

--- BC_scripts/wait.sh
+++ BC_scripts/wait.sh
@@ -14,7 +14,7 @@ send_formatted_message() {
 
     response=$(curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
     -d "chat_id=$CHAT_ID" \
-    -d "text=$message" \
+    -d "text=$formatted_message" \
     -d "parse_mode=MarkdownV2")
     # Extract message ID from response and save it to a variable
     message_id=$(echo "$response" | jq -r '.result.message_id')    
